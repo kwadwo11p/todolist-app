410fa84947309b8e709a4c4339a40ad162199261
Kwadwo <kwadwo11wk@gmail.com>
2022-05-26 18:51:58 -0400

    first commit

---
+++ todo.js
@@ -0,0 +1,38 @@
+// 
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+
+// Generate new todo list
+const generateTemplate = todo => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+    </li>
+`;
+    list.innerHTML += html;
+};
+
+
+// Add todo
+addForm.addEventListener('submit', e => {
+
+e.preventDefault();
+const todo = addForm.add.value.trim();
+
+if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+    }
+
+});
+
+// delete todos
+list.addEventListener('click', e => {
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+
+    });
